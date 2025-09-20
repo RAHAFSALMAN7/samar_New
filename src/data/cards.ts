1653229aa9899c6de016815a15603d58c242ea2e
Rahafsalman <144127007+RAHAFSALMAN7@users.noreply.github.com>
2025-09-20 10:25:25 +0300

    Initial commit

---
+++ src/data/cards.ts
@@ -0,0 +1,15 @@
+export const cardsData = {
+    mn_al_akher: [
+        "/src/assets/cards/mn_al_akher/card1.png",
+        "/src/assets/cards/mn_al_akher/card2.png",
+        "/src/assets/cards/mn_al_akher/card3.png",
+        "/src/assets/cards/mn_al_akher/card4.png",
+        "/src/assets/cards/mn_al_akher/card5.png",
+        "/src/assets/cards/mn_al_akher/card6.png",
+        "/src/assets/cards/mn_al_akher/card7.png",
+        "/src/assets/cards/mn_al_akher/card8.png",
+        "/src/assets/cards/mn_al_akher/card9.png",
+        "/src/assets/cards/mn_al_akher/card10.png",
+    ],
+    // باقي السكشنز...
+};
